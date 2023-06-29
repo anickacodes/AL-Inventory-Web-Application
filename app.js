57daee181b714f73085e3424341af2aba3c04756
anickacodes <anickalewis@pursuit.org>
2023-06-29 02:36:23 -0400

    added directory.html for full client list, included navs on both htmls, styled .new-clients-added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// document.getElementById('date').value = Date();
+document.addEventListener("DOMContentLoaded", (event) => {
+  const loginForm = document.getElementById("login-form");
+
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    alert(
+      "Functionality in progress\nPlease continue to creating a new profile"
+    );
+  });
+});
