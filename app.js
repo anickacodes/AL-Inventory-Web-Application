978c2893afe042a8a8ad7ac2bd74e44b17eaac6d
anickacodes <anickalewis@pursuit.org>
2023-06-29 03:41:27 -0400

    added alert, worked on left grid (added events for clearing all inputs and switch

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const loginForm = document.getElementById("login-form");
+  const loginForm = document.getElementById("log-in-form");
+  const occupationInput = document.getElementById("occupation");
+  const nameInput = document.getElementById("name");
+  const switchInput = document.querySelector(".switch input");
+  const clearBtn = document.getElementById("create");
+  const imageSelector = document.querySelector(".dropdown");
 
   loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -8,4 +13,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
       "Functionality in progress\nPlease continue to creating a new profile"
     );
   });
+
+  switchInput.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      clearBtn.addEventListener("click", clearInputs);
+    } else {
+      clearBtn.removeEventListener("click", clearInputs);
+    }
+  });
+  function clearInputs() {
+    switchInput.checked = false;
+    nameInput.value = "";
+    occupationInput.value = "";
+    imageSelector.value = "";
+  }
 });
+
+// function resetStatus(resetButton) {
+//     const statusInput = resetButton.parentNode.querySelector('input[type="radio"]');
+//     statusInput.checked = true;
+// }
