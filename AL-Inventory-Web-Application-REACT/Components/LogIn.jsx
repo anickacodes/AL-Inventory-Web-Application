a7a2eb61bc070a3da21b6220171d503b9456f2e7
anickacodes <anickalewis@pursuit.org>
2023-08-01 01:50:36 -0400

    imported LogIn form component & component folder

---
+++ AL-Inventory-Web-Application-REACT/Components/LogIn.jsx
@@ -0,0 +1,26 @@
+
+
+
+
+const LogIn = () => {
+
+//     <div className="login-container">
+
+// <form id="log-in-form" action="" method="post">
+//     <label for="e-mail">Email</label>
+//     <input id="e-mail" type="text" placeholder="E-mail" name="e-mail">
+//     <label for="password">Password:</label>
+//     <input id="password" type="password" placeholder="Password" name="password">
+//     <button type="submit">Login</button>
+// </form>
+// </div>
+return(<>
+    <label htmlFor="e-mail">Email</label>
+    <input id="e-mail" type="text" placeholder="E-mail" name="e-mail" />
+    <label htmlFor="password">Password:</label>
+    <input id="password" type="password" placeholder="Password" name="password" />
+    <button type="submit">Login</button>
+</>
+)}
+
+export default LogIn
